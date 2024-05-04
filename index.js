18de2baa6f8b58694aca9b2f8491629854324298
HackNetAyush <48079328+HackNetAyush@users.noreply.github.com>
2024-05-05 03:56:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const app = express();
+app.use(express.json()); // Parse JSON requests
+app.use(cors());
+
+const MODEL_NAME = "gemini-1.0-pro";
+// const API_KEY = "AIzaSyCmeFeG8eVBM_x0zYxrYWx0W9A-RMHlRig";
+
+async function runChat(api, userInput) {
+  const genAI = new GoogleGenerativeAI(api);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  });
+
+  const instructions =
+    " \n Only provide me code, don't explain if I have not mentioned 'explain' ";
+
+  var completeMsg = userInput + instructions;
+
+  const result = await chat.sendMessage(completeMsg);
+  const response = result.response;
+  return response.text();
+}
+
+app.get("/:api/message", async (req, res) => {
+  const API = req.params.api;
+  const userInput = req.query.input; // Change to req.body.input if sending input in the request body
+  try {
+    const chatResponse = await runChat(API, userInput);
+    res.json({ message: chatResponse });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
